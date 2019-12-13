3ca033ed304259d5c1b83a37782ef5ee22e199f0
段小辉 <2283330272@qq.com>
2019-12-13 19:47:20 +0800

    初始化项目

---
+++ src/test/java/cc/weno/blockchain/BlockchainApplicationTests.java
@@ -0,0 +1,13 @@
+package cc.weno.blockchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockchainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
